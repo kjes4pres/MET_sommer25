f61ec48d6c7b0dd3a2aa67a16c0d4263d01a436d
Kjersti Stangeland <stangelandkjersti@gmail.com>
2025-07-15 12:34:57 +0000

    updated outputfile string

--- find_deformation_radius.py
+++ find_deformation_radius.py
@@ -21,7 +21,7 @@ import time
 from density import dens
 
 def main():
-    """ files = glob('/lustre/storeB/project/nwp/havvind/hav/results/reference/')
+    files = glob('/lustre/storeB/project/nwp/havvind/hav/results/reference/')
     months = {
     "02": 27,  # February
     "03": 31,  # March
@@ -34,9 +34,9 @@ for month, days in months.items():
     for day in range(1, days + 1): 
         day_str = f"{day:04}"
         file_path = f'/REF-{month}/norkyst_avg_{day_str}.nc'
-        files.append(file_path)"""
+        files.append(file_path)
 
-    files = glob('/lustre/storeB/project/nwp/havvind/hav/results/reference/REF-06/norkyst_avg_0001.nc')
+    #files = glob('/lustre/storeB/project/nwp/havvind/hav/results/reference//REF-06/norkyst_avg_0001.nc')
     for f in files:
         print(f)
         bvf_calc(f)
@@ -48,42 +48,50 @@ def bvf_calc(mfile):
     grid = SGrid(ds) 
 
     # Depths to interpolate to
-    """ zlevs = np.arange(0,51,1)
+    # Depth levels from Annks
+    zlevs = np.arange(0,51,1)
     zlevs = np.insert(zlevs,len(zlevs),values=np.arange(52,102,2), axis =0)
     zlevs = np.insert(zlevs,len(zlevs),values=np.arange(105,305,5), axis =0)
     zlevs = np.insert(zlevs,len(zlevs),values=np.arange(520,1020,20), axis =0)
-    zlevs = np.insert(zlevs,len(zlevs),values=np.arange(1050,3050,50), axis =0) """
-
-    zlevs = np.arange(-3000, -520, 20)
-    zlevs = np.insert(zlevs, len(zlevs), values=np.arange(-300, -105, 5), axis=0)
-    zlevs = np.insert(zlevs, len(zlevs), values=np.arange(-100, -52, 2), axis=0)
-    zlevs = np.insert(zlevs, len(zlevs), values=np.arange(-50, 1, 1), axis=0)
+    zlevs = np.insert(zlevs,len(zlevs),values=np.arange(1050,3050,50), axis =0)
 
     zlevs = zlevs[np.where(zlevs<=np.max(grid.h))]
-    #zlevs = np.array(zlevs)*-1.
+    zlevs = np.array(zlevs)*-1.
     
-    # Depth of rho-points
+    # Depth of rho-points: z_r
+    # Adding zero to end of array (surface)
     z_r = np.insert(grid.z_r, grid.z_r.shape[0], values = np.zeros_like(grid.h), axis=0)
+    # Adding local water depth to beginning of array
     z_r = np.insert(z_r, 0, values = -1.*grid.h, axis=0)
 
     # Variables
     ocean_time = ds.variables['ocean_time']
-    density = ds.variables['rho'][:] + 1000  # Full density - not just anomaly
+    #density = ds.variables['rho'][:] + 1000  # Full density - not just anomaly
     salt  = ds.variables['salt'][:]
     temp = ds.variables['temp'][:]
     f = ds.variables['f'][:]
-    h = grid.h[:,:]
+    #h = grid.h[:,:]
     mask = grid.mask_rho[:,:]
 
-    tmpn = np.ones([1, density.shape[2], density.shape[3]])*np.nan  # Temporary array holding buoyancy frequency
-    tmpr = np.ones([1, density.shape[2], density.shape[3]])*np.nan  # Temporary array holding Rossby radius
+    tmpn = np.ones([1, salt.shape[2], salt.shape[3]])*np.nan  # Temporary array holding integrated buoyancy frequency
+    tmpr = np.ones([1, salt.shape[2], salt.shape[3]])*np.nan  # Temporary array holding Rossby radius
 
     # Making a file to write to
-    outputf = '/home/kjsta7412/sommer_25/MET_sommer25/output_bdr/' + mfile.split('/')[-1].replace('.nc','NEWTRYZLEVS.nc')
+    #outputf = '/home/kjsta7412/sommer_25/MET_sommer25/output_bdr/' + mfile.split('/')[-1].replace('.nc',f'__brr.nc')
+
+    ref_part = mfile.split('reference//')[-1].split('/')[0].replace('-', '_')  # 'REF-02' to 'REF_02'
+    norkyst_part = mfile.split('/')[-1][:-3]  # Get 'norkyst_avg_0001' without '.nc'
+    filename = f"{ref_part}_{norkyst_part}_brr.nc"
+    outputf = '/home/kjsta7412/sommer_25/MET_sommer25/output_bdr/' + filename
+
     rootgrp = Dataset(outputf, 'w')
     time =  rootgrp.createDimension("ocean_time", None)
     X = rootgrp.createDimension("xi_rho", len(ds.dimensions['xi_rho']))
     Y = rootgrp.createDimension("eta_rho", len(ds.dimensions['eta_rho']))
+
+    #ref = datetime(1970, 1, 1)  # Reference time
+    #ocean_time = np.array(ocean_time)
+    #date = pd.to_datetime(ref + timedelta(seconds=ocean_time[0]))
     otime = rootgrp.createVariable("ocean_time","f8",("ocean_time",), zlib=True)
     otime[:] = ocean_time[-1]
 
@@ -99,29 +107,34 @@ def bvf_calc(mfile):
     ds.close()
 
     # Looping through grid points
-    for y in range(0, density.shape[2]):
-        for x in range(0, density.shape[3]): 
+    for y in range(0, salt.shape[2]):
+        for x in range(0, salt.shape[3]): 
             if not mask[y, x]:  # skipping land points
                continue
 
             tmpn[0,y,x] = 0    
+            # Filtering out local water depth on the zlevs
+            # Where zlevs is shallower than z_r -> true
             tmpnz = zlevs[np.where(zlevs[:]>z_r[:,y,x].min())].squeeze()  # z-levels to calculate N on
             N_depth = (tmpnz[0:-1] + tmpnz[1:])/2.  # Midpoints between z-levels
 
             if 1:
                t = -1
-               #tmpnS = salt[t,:,y,x]; tmpnS = np.append(tmpnS,tmpnS[-1]); tmpnS = np.insert(tmpnS,0,values=tmpnS[0],axis=0)
-               #tmpnT = temp[t,:,y,x]; tmpnT = np.append(tmpnT,tmpnT[-1]); tmpnT = np.insert(tmpnT,0,values=tmpnT[0],axis=0)
-               #tempZ = griddata(z_r[:,y,x], tmpnT[:], tmpnz)
-               #saltZ = griddata(z_r[:,y,x], tmpnS[:], tmpnz)
+               # Adding first and last value to each end of the array to match length of z-levels (tmpnz)
+               tmpnS = salt[t,:,y,x]; tmpnS = np.append(tmpnS,tmpnS[-1]); tmpnS = np.insert(tmpnS,0,values=tmpnS[0],axis=0)
+               tmpnT = temp[t,:,y,x]; tmpnT = np.append(tmpnT,tmpnT[-1]); tmpnT = np.insert(tmpnT,0,values=tmpnT[0],axis=0)
+               # Interpolating to z-levels
+               tempZ = griddata(z_r[:,y,x], tmpnT[:], tmpnz)
+               saltZ = griddata(z_r[:,y,x], tmpnS[:], tmpnz)
 
-               tmpD =  density[t, :, y, x]
-               tmpD = np.append(tmpD, tmpD[-1]); tmpD = np.insert(tmpD, 0, values=tmpD[0], axis=0)
-               densZ = griddata(z_r[:, y, x], tmpD[:], tmpnz) 
+               #tmpD =  density[t, :, y, x]
+               #tmpD = np.append(tmpD, tmpD[-1]); tmpD = np.insert(tmpD, 0, values=tmpD[0], axis=0)
+               #densZ = griddata(z_r[:, y, x], tmpD[:], tmpnz) 
 
-               #densm = dens(saltZ, tempZ, np.zeros_like(tempZ)).squeeze()
-               #n2 = N2(densm, tmpnz);  n2[np.where(n2<0)] = 0.0000001; N = np.sqrt(n2).squeeze()
-               n2 = N2(densZ, tmpnz); n2[np.where(n2<0)] = 0.0000001; N = np.sqrt(n2).squeeze()
+               densm = dens(saltZ, tempZ, np.zeros_like(tempZ)).squeeze()
+               # Calculating the buoyancy frequency
+               n2 = N2(densm, tmpnz);  n2[np.where(n2<0)] = 0.0000001; N = np.sqrt(n2).squeeze()
+               #n2 = N2(densZ, tmpnz); n2[np.where(n2<0)] = 0.0000001; N = np.sqrt(n2).squeeze()
 
                # Integrating N over z-levels
                for k in range(0, len(N)-1):
