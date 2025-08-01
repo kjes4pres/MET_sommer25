f02f00bc8261b1aa5d6c8c9baf45992c6a29de86
Kjersti Stangeland <stangelandkjersti@gmail.com>
2025-08-01 11:25:28 +0000

    cleanup and comments

--- Rossby_deformation/find_deformation_radius.py
+++ Rossby_deformation/find_deformation_radius.py
@@ -1,8 +1,7 @@
 """
----Kjersti Stangeland, July 2025---
-
-Module for reading in reference data from Norkyst v3 (daily files),
-interpolating density from s-levels to z-depths, 
+Module for reading in data from Norkyst v3 (daily files),
+interpolating salinity and temperature from s-levels to z-depths,
+calculating the potential density on z-depths, 
 calculating buoyancy frequency on z-depths,
 calculating Baroclinic Rossby radius (mode 1) via WKB approximation,
 and lastly writing the radius and integrated buoyancy frequency to a netcdf file.
@@ -10,10 +9,11 @@ and lastly writing the radius and integrated buoyancy frequency to a netcdf file
 Code and logic strongly inspired by A. K. Sperrevik:
 '/home/annks/Projects/LoVe'
 
-References:
 Sperrevik, A. K., J. Röhrs, and K. H. Christensen (2017), 
 Impact of data assimilation on Eulerian versus Lagrangian estimates of upper ocean transport, 
 J. Geophys. Res. Oceans, 122, 5445–5457, doi:10.1002/2016JC012640.
+
+July 2025, Kjersti Stangeland (kjersti.stangeland@met.no)
 """
 
 from roppy import SGrid
@@ -27,6 +27,7 @@ from Rossby_deformation.density import dens
 
 
 def main():
+    # Making a list of paths to files
     base_path = glob("/lustre/storeB/project/nwp/havvind/hav/results/experiment/")
     months = {
         "02": 27,  # February
@@ -44,7 +45,6 @@ def main():
             file_path = f"/EXP-{month}/norkyst_avg_{day_str}.nc"
             files.append(base_path[0] + file_path)
 
-    # files = glob('/lustre/storeB/project/nwp/havvind/hav/results/reference//REF-06/norkyst_avg_0001.nc')
     for f in files:
         print(f)
         bvf_calc(f)
@@ -57,7 +57,6 @@ def bvf_calc(mfile):
     grid = SGrid(ds)
 
     # Depths to interpolate to
-    # Depth levels from Annks
     zlevs = np.arange(0, 51, 1)
     zlevs = np.insert(zlevs, len(zlevs), values=np.arange(52, 102, 2), axis=0)
     zlevs = np.insert(zlevs, len(zlevs), values=np.arange(105, 305, 5), axis=0)
@@ -75,38 +74,28 @@ def bvf_calc(mfile):
 
     # Variables
     ocean_time = ds.variables["ocean_time"]
-    # density = ds.variables['rho'][:] + 1000  # Full density - not just anomaly
     salt = ds.variables["salt"][:]
     temp = ds.variables["temp"][:]
     f = ds.variables["f"][:]
-    # h = grid.h[:,:]
     mask = grid.mask_rho[:, :]
 
-    tmpn = (
-        np.ones([1, salt.shape[2], salt.shape[3]]) * np.nan
-    )  # Temporary array holding integrated buoyancy frequency
-    tmpr = (
-        np.ones([1, salt.shape[2], salt.shape[3]]) * np.nan
-    )  # Temporary array holding Rossby radius
+    # Temporary array holding integrated buoyancy frequency
+    tmpn = (np.ones([1, salt.shape[2], salt.shape[3]]) * np.nan)
+    # Temporary array holding Rossby radius
+    tmpr = (np.ones([1, salt.shape[2], salt.shape[3]]) * np.nan)
 
-    # Making a file to write to
-    # outputf = '/home/kjsta7412/sommer_25/MET_sommer25/output_bdr/' + mfile.split('/')[-1].replace('.nc',f'__brr.nc')
-
-    ref_part = (
-        mfile.split("experiment//")[-1].split("/")[0].replace("-", "_")
-    )  # 'REF-02' to 'REF_02'
+    # Making a filepath to write to
+    ref_part = (mfile.split("experiment//")[-1].split("/")[0].replace("-", "_"))  # 'REF-02' to 'REF_02'
     norkyst_part = mfile.split("/")[-1][:-3]  # Get 'norkyst_avg_0001' without '.nc'
     filename = f"{ref_part}_{norkyst_part}_brr.nc"
     outputf = "/lustre/storeB/users/kjsta7412/output_bdr/EXP/" + filename
 
+    # Making the outputfile
     rootgrp = Dataset(outputf, "w")
     time = rootgrp.createDimension("ocean_time", None)
     X = rootgrp.createDimension("xi_rho", len(ds.dimensions["xi_rho"]))
     Y = rootgrp.createDimension("eta_rho", len(ds.dimensions["eta_rho"]))
 
-    # ref = datetime(1970, 1, 1)  # Reference time
-    # ocean_time = np.array(ocean_time)
-    # date = pd.to_datetime(ref + timedelta(seconds=ocean_time[0]))
     otime = rootgrp.createVariable("ocean_time", "f8", ("ocean_time",), zlib=True)
     otime[:] = ocean_time[-1]
 
@@ -162,20 +151,17 @@ def bvf_calc(mfile):
                 tmpnT = temp[t, :, y, x]
                 tmpnT = np.append(tmpnT, tmpnT[-1])
                 tmpnT = np.insert(tmpnT, 0, values=tmpnT[0], axis=0)
+
                 # Interpolating to z-levels
                 tempZ = griddata(z_r[:, y, x], tmpnT[:], tmpnz)
                 saltZ = griddata(z_r[:, y, x], tmpnS[:], tmpnz)
 
-                # tmpD =  density[t, :, y, x]
-                # tmpD = np.append(tmpD, tmpD[-1]); tmpD = np.insert(tmpD, 0, values=tmpD[0], axis=0)
-                # densZ = griddata(z_r[:, y, x], tmpD[:], tmpnz)
-
                 densm = dens(saltZ, tempZ, np.zeros_like(tempZ)).squeeze()
+
                 # Calculating the buoyancy frequency
                 n2 = N2(densm, tmpnz)
                 n2[np.where(n2 < 0)] = 0.0000001
                 N = np.sqrt(n2).squeeze()
-                # n2 = N2(densZ, tmpnz); n2[np.where(n2<0)] = 0.0000001; N = np.sqrt(n2).squeeze()
 
                 # Integrating N over z-levels
                 for k in range(0, len(N) - 1):
